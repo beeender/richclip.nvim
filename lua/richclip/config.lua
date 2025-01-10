2daa8d6e1653830fba08c0c6196ccd75c32a3c4e
Chen Mulong <chenmulong@gmail.com>
2025-01-10 15:53:54 +0800

    Take over the clipboard by default

--- lua/richclip/config.lua
+++ lua/richclip/config.lua
@@ -1,11 +1,11 @@
 local CONFIG = {}
 
 CONFIG.with_defaults = function(options)
-    --- Specify the richclip executable path. If it is nil, the plugin will try to download the
+    --- Specify the richclip executable path. If it is nil, the plugin will try to download
     --- it automatically.
     CONFIG.richclip_path = options.richclip_path or nil
     --- Set g:clipboard to let richclip take over the clipboard.
-    CONFIG.set_g_clipboard = options.set_g_clipboard or false
+    CONFIG.set_g_clipboard = options.set_g_clipboard or true
     --- To print debug logs
     CONFIG.enable_debug = options.debug or false
 end
